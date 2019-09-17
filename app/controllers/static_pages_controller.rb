c445aee75e2693b3b647a5a89ca5a6c9ac4a797a
Powai Leung <pwleung1489@gmail.com>
2019-09-17 02:28:14 +0000

    input Flixter Lesson 2-4 Landing page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+# connect our landing page to the index action inside the static_pages controller
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # hook up the routes so the localhost:3030 takes us to the landing page
+  root 'static_pages#index'
 end
