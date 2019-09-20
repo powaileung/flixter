0e98716ecaf0f64b41e5837e1fd46bda93fc02b5
Powai Leung <pwleung1489@gmail.com>
2019-09-20 02:30:21 +0000

    input Flixter Lesson 2-11 Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # hook up the routes so the localhost:3030 takes us to the landing page
   root 'static_pages#index'
