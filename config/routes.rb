aa46dec6ba040b1ad5b429a32b404a6920b32aed
Powai Leung <pwleung1489@gmail.com>
2019-09-22 05:17:41 +0000

    input Flixter Lesson 3-18 Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
-      # we hook the new and create actions by "nesting" the sections underneath the course resource
-      resources :sections, only: [:new, :create]
+      # we hook the new and create actions by "nesting" the sections underneath the courses resource
+      resources :sections, only: [:new, :create] do
+        # we hook the new and create actions by "nesting" the lessons underneath the sections resource
+        resources :lessons, only: [:new, :create]
+      end
     end
   end
 end
